aa8fbf0fd2c633a1f6153ad62d6c9e46238a333e
blamersj30 <152201467+blamersj30@users.noreply.github.com>
2024-02-23 17:52:00 -0600

    Update project2.js

--- project2.js
+++ project2.js
@@ -24,37 +24,35 @@ client.on('loggedOn', () => {
   console.log('Logged into Steam');
 
   client.setPersona(SteamUser.EPersonaState.Online);
-  client.gamesPlayed(440);
+  client.gamesPlayed(730);
 });
 
 client.on('webSession', (sessionid, cookies) => {
   manager.setCookies(cookies);
-
   community.setCookies(cookies);
   community.startConfirmationChecker(10000, config.idSecret);
-
   console.log('Trade Offer Manager initialized successfully.');
 });
 
 manager.on('newOffer', offer => {
   console.log('Trade offer detected...')
-    if (offer.partner.getSteamID64() === 'your_trusted_account_id') {
-      console.log('Trade offer from trusted account detected...')
-      
-      offer.accept((err, status) => {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log(`Accepted offer. Status: ${status}.`);
-        }
-      });
-    } else {
-      offer.decline(err => {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log('Canceled offer from scammer.');
-        }
-      });
-    }
-  });
+  if (offer.partner.getSteamID64() === 'your_trusted_account_id') {
+    console.log('Trade offer from trusted account detected...')
+    
+    offer.accept((err, status) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(`Accepted offer. Status: ${status}.`);
+      }
+    });
+  } else {
+    offer.decline(err => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Cancelled offer from scammer.');
+      }
+    });
+  }
+});
